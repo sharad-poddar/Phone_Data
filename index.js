0b9e3cc1d66ca059d2a5ce983b2c23a902ce3474
shared <sharadpoddar1001@gmail.com>
2024-01-28 22:55:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,109 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+// express.json() used to get body of any req
+app.use(express.json());
+app.use(morgan('tiny'));
+
+const data = [
+    {
+        id: 1,
+        name: 'Rahul',
+        phone: '123456789'
+    },{
+        id: 2,
+        name: 'naman',
+        phone: '178900000'
+    },{
+        id: 3,
+        name: 'arman',
+        phone: '120000089'
+    }
+]
+
+// Data was tranfered in form of string
+app.get('/',(req, res)=>{
+    res.send('<p>welcome to Phone Diary</p>');
+})
+
+app.get('/info', (req, res)=>{
+    res.send(`
+        <div>
+            <h2>developed by Sharad Poddar</h2>
+            <p>Learning Full Stack Development</p>
+            <p>Phone No: +910000111192</p>
+        </div>
+    `)
+})
+
+// data comes in json format
+app.get('/api/data',(req, res)=>{
+    res.json(data);
+})
+
+// data sending on specific id
+app.get('/api/data/:id',(req, res)=>{
+    const id = req.params.id;
+
+    if(data.length == 0){
+        res.send('<p>No data is available currently!</p>')
+    }
+
+    const content = data.find(e=>e.id == id);
+
+    if(!content){
+        res.status(404).end();
+    }else{
+        res.json(content);
+    }
+})
+
+// POST the data to server
+app.post('/api/data',(req, res)=>{
+    if(!req.body.name || !req.body.phone || req.body.name==''){
+        return res.status(400).end();
+    }
+
+    const same_name_data = data.find(e=>e.name == req.body.name);
+    if(same_name_data){
+        return res.status(400).send(`<div>
+            <h2>Error</h2>    
+            <p>please add some unique name<p>
+        </div>`);
+    }
+
+    const phone_data = {
+        id: data.length+1,
+        name: req.body.name,
+        phone: req.body.phone
+    }
+
+    data.push(phone_data);
+    console.log(data);
+    res.status(200).json(data);
+})
+
+// DELETE the any specific id 
+app.delete('/api/data/:id',(req, res)=>{
+    const id = req.params.id;
+    const newData = data.filter(e=>id != e.id);
+    console.log(newData);
+    res.send('<p>deleted</p>')
+})
+
+
+// user defined middleware
+app.use((req, res)=>{
+    res.status(404).json({"error": "unknown content"});
+})
+
+
+const PORT = 4000 || process.env.PORT;
+app.listen(PORT, ()=>{
+    console.log(`server is listening at PORT ${PORT}`);
+})
